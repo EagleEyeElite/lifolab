fa697041921bfe94a243a7e059879ae32bacdd3b
ckl <conrad.klaus@outlook.de>
2025-06-15 15:42:02 +0200

    fix navbar text size

--- frontend/src/app/navbar.tsx
+++ frontend/src/app/navbar.tsx
@@ -10,23 +10,23 @@ export default function Navbar() {
 
   return (
     <>
-    <nav className="sticky top-0 z-10 py-6 flex justify-between items-center border-black text-black text-lg font-light">
-      <div className="w-40 h-5 " />
-      <h1 className="border p-0.5 px-4 bg-[rgba(0,255,94,0.91)] rounded-3xl text-center">
-        Living the Forest Lab
-      </h1>
-      <ul className="flex gap-3 justify-end items-center border border-r-0 rounded-l-3xl p-0.5 pl-4 bg-[rgba(0,255,94,0.91)]">
-        {navLinks.map((link) => (
-          <li key={link}>
-            <Link href={`#${link.toLowerCase()}`}>
-              {link}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-    <div className="min-h-screen bg-lime-100" ref={containerRef}/>
-    <ScrollAnimatedLogo containerRef={containerRef}/>
+      <nav className="sticky top-0 z-10 py-6 flex justify-between items-center border-black text-black font-mono ">
+        <div className="w-40 h-5" />
+        <h1 className="border p-0.5 px-4 bg-[rgba(0,255,94,0.91)] rounded-3xl text-center">
+          Living the Forest Lab
+        </h1>
+        <ul className="flex gap-3 justify-end items-center border border-r-0 rounded-l-3xl p-0.5 pl-4 bg-[rgba(0,255,94,0.91)]">
+          {navLinks.map((link) => (
+            <li key={link}>
+              <Link href={`#${link.toLowerCase()}`}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="min-h-screen bg-lime-100" ref={containerRef}/>
+      <ScrollAnimatedLogo containerRef={containerRef}/>
     </>
   );
 }
