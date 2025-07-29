180538678625b6a7fc1747dafa386ff2141f9bf1
ckl <conrad.klaus@outlook.de>
2025-07-29 16:36:55 +0200

    wip

--- frontend/src/components/ui/sectionHeader.tsx
+++ frontend/src/components/ui/sectionHeader.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { LucideIcon } from "lucide-react";
 
-export default function SectionHeader({
-                                        children,
-                                        icon: Icon,
-                                        className = ""
-                                      }: {
+export default function SectionHeader({children, icon: Icon,}: {
   children: React.ReactNode;
   icon?: LucideIcon;
-  className?: string;
 }) {
   return (
-    <div className={`border-y border-black py-3 ${className}`}>
+    <div className={`border-y border-black py-3`}>
       <h2 className="text-lg pl-3 font-heading tracking-wide flex items-center gap-2">
         {children} {Icon && <Icon size={18} />}
       </h2>
