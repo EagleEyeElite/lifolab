2349831c8a721f01b821a53dd257359add677925
ckl <conrad.klaus@outlook.de>
2025-07-29 16:14:22 +0200

    wip: simplify tagList

--- frontend/src/components/ui/TagList.tsx
+++ frontend/src/components/ui/TagList.tsx
@@ -1,6 +1,6 @@
 import { graphqlClient } from '@/graphql/client';
 import { gql } from 'graphql-request';
-import TagPill from './tagPill';
+import Link from 'next/link';
 
 const GetTagsBySlugs = gql`
   query GetTagsBySlugs($slugs: [String!]!) {
@@ -16,35 +16,26 @@ const GetTagsBySlugs = gql`
   }
 `;
 
-interface TagListProps {
+export default async function TagList({ tagSlugs }: {
   tagSlugs: string[];
-}
+}) {
+  const { tags } = await graphqlClient.request<any>(GetTagsBySlugs, { slugs: tagSlugs });
 
-export default async function TagList({ tagSlugs }: TagListProps) {
-  if (!tagSlugs.length) {
-    return null;
+  if (!tags?.edges?.length) {
+    throw new Error(`Error fetching tags for tags ${tagSlugs}`);
   }
 
-  try {
-    const { tags } = await graphqlClient.request<any>(GetTagsBySlugs, { slugs: tagSlugs });
-    
-    if (!tags?.edges?.length) {
-      return null;
-    }
-
-    return (
-      <div className="flex flex-wrap gap-1">
-        {tags.edges.map(({ node: tag }: any) => (
-          <TagPill
-            key={tag.id}
-            name={tag.name}
-            href={`/tags/${tag.slug}`}
-          />
-        ))}
-      </div>
-    );
-  } catch (error) {
-    console.error('Failed to fetch tags:', error);
-    return null;
-  }
+  return (
+    <div className="flex flex-wrap gap-1">
+      {tags.edges.map(({ node: tag }: any) => (
+        <Link
+          key={tag.id}
+          href={`/tags/${tag.slug}`}
+          className="inline-flex items-center px-2 py-1 text-xs font-heading bg-secondary border border-black rounded-full"
+        >
+          {tag.name}
+        </Link>
+      ))}
+    </div>
+  );
 }
